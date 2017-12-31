1653521f672010c3cfdc5a4849031709e7588fe0
DESKTOP-S7O6GUK\Tobip <Tobias.prause@gmx.de>
2017-12-31 01:27:17 +0100

    Änderungen von Lange übernommen

--- src/Controller.java
+++ src/Controller.java
@@ -10,9 +10,17 @@ public class Controller {
 		windowcontroller.setTitle("Controller");
 		JPanel controllerpanel = new JPanel();
 		
-		JButton ok_button = new JButton("OK");
+		JButton rotate_left_button = new JButton("Rotate_Left");
 		
-		controllerpanel.add(ok_button);
+		controllerpanel.add(rotate_left_button);
+		
+		JButton rotate_right_button = new JButton("Rotate_Right");
+		
+		controllerpanel.add(rotate_right_button);
+		
+		
+		
+		
 		
 		windowcontroller.add(controllerpanel);
 		
