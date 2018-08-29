3a2f781a75f2c9e006500483d6f028eff1ff5542
Killerbeast01 <34600025+Killerbeast01@users.noreply.github.com>
2018-08-29 23:05:39 +0200

    Neues GUI mit FXML erstelllt;

--- src/Controller.java
+++ src/Controller.java
@@ -1,34 +1,65 @@
-import javax.swing.*;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 
-public class Controller extends JFrame {
+public class Controller{
 
-    private JPanel rootpanel;
-    private JButton btnaltesschulgebaeude;
-    private JButton btnottohahn;
-    private JButton btnneuesschulgebaeude;
-    private JButton btnrotateleft;
-    private JButton btnrotateright;
-    private JButton btnrotationreset;
+    @FXML
+    Button btn_old_school, btn_recent_school, btn_otto, btn_rotate_reset;
 
-    public Controller() {
+    @FXML
+    Button btn_rotate_left, btn_rotate_right;
 
-        super("Controller");
-        setContentPane(rootpanel);
-        pack();
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    @FXML
+    void btn_old_school() {
 
-        btnaltesschulgebaeude.addActionListener(actionEvent -> Main.showoldschool());
-        btnneuesschulgebaeude.addActionListener(actionEvent -> Main.showrecentschool());
-        btnottohahn.addActionListener(actionEvent -> Main.showottohahn());
-        btnrotateleft.addActionListener(actionEvent -> Main.rotateleft());
-        btnrotateright.addActionListener(actionEvent -> Main.rotateright());
-        btnrotationreset.addActionListener(actionEvent -> Main.rotatereset());
+        btn_old_school.setFont(Font.font("Verdana", FontWeight.BOLD, 10));
+        btn_otto.setFont(null);
+        btn_recent_school.setFont(null);
+        Main.showoldschool();
 
-        setVisible(true);
+    }
 
-        Main.startup();
+    @FXML
+    void btn_recent_school() {
 
-    } //Erzeugt das Bedienfeld des Hologramms
+        btn_recent_school.setFont(Font.font("Verdana", FontWeight.BOLD, 10));
+        btn_otto.setFont(null);
+        btn_old_school.setFont(null);
+        Main.showrecentschool();
+
+    }
+
+    @FXML
+    void btn_otto() {
+
+        btn_otto.setFont(Font.font("Verdana", FontWeight.BOLD, 10));
+        btn_old_school.setFont(null);
+        btn_recent_school.setFont(null);
+        Main.showottohahn();
+
+    }
+
+    @FXML
+    void btn_rotate_reset() {
+
+        Main.rotatereset();
+
+    }
+
+    @FXML
+    void btn_rotate_right() {
+
+        Main.rotateright();
+
+    }
+
+    @FXML
+    void btn_rotate_left() {
+
+        Main.rotateleft();
+
+    }
 
 }
-
